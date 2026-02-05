37bffc8cf6fd337dd0a3a27851f21d5c80e1ca23
xaerinoo <xaerinoo@naver.com>
2026-02-06 02:09:12 +0900

    Feat: 라우팅 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/Home/HomePage"
+import HistoryPage from "./pages/History/HistoryPage"
+
 function App() {
   return (
-    <>
-      <h1>Chatbot</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
